8f37077bf3aedb8fc2c2f182b0bebe050aab7c5f
antonhansel <antoninribeaud@gmail.com>
2014-12-08 15:23:57 +0100

    Heroku test commit

--- app/models/message.js
+++ app/models/message.js
@@ -11,6 +11,5 @@ var userSchema = mongoose.Schema({
 });
 
 // methods ======================
-
 // create the model for users and expose it to our app
 module.exports = mongoose.model('Message', userSchema);
--- app/slack.js
+++ app/slack.js
@@ -1,3 +1,13 @@
+var Message = require('./models/message.js');
+
 exports.saveMessage = function(req, res){
-	console.log(req.body);
+	var message = new Message();
+	message.channel_name = req.body.channel_name;
+	message.user_name = req.body.user_name;
+	message.text = req.body.text;
+	message.timestamp = req.body.timestamp;
+	message.save(function(err){
+		if (err) throw err;
+		return;
+	});
 }
