ae97441ee5f6cf07adf5da81a5a1eebbd808372b
antonhansel <antoninribeaud@gmail.com>
2014-12-24 16:31:00 +0100

    Refactoring index.ejs

---
+++ public/dist/js/crawler.js
@@ -0,0 +1,31 @@
+jQuery(function () {
+    $('#srch-button').click(function(){
+        $.ajax({
+            type: "POST",
+            url: '/crawlsoundcloud',
+            data: {'url' : document.getElementById('srch-term').value},
+            success: function(body){
+                $('#result').empty();
+                if (body.error != 'none' && body.error != undefined){
+                    $('#result').append("Error: " + body.error + "</br>");
+                }
+                else {
+                    $('#result').append("Right click on the songs and select \"Save link as\"...</br>");
+                    if (body.url != undefined){
+                        $('#result').append("<a href=\"" + body.url + "\" download=\"" + 
+                            body.title + "\"> " + body.title + "</a></br>");
+                    } else {
+                        for (var i = 0; i < body.length; i++){
+                            if (body[i].url){
+                                $('#result').append("<a href=\"" + body[i].url + "\" download=\"" + body[i].title + "\">"+ body[i].title + "</a></br>");
+                            }
+                            else {
+                                $('#result').append("Cannot generate download link</br>");
+                            }
+                        }
+                    }
+                }
+            },
+        });
+    });
+});
