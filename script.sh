21f71d90326f10ae2f862afa08814d12c38f2088
antonhansel <antoninribeaud@gmail.com>
2015-01-04 00:11:40 +0100

    Removing unused npm module

--- script.sh
+++
@@ -1,9 +0,0 @@
-git add app --all
-git add views --all
-git add config --all
-git add public --all
-git add server.js
-git add script.sh
-git commit -m "Heroku test commit"
-git push heroku master
-git push origin master
