5e951c57540284fd0c6ac5a3bda55b4203b047d3
antonhansel <antoninribeaud@gmail.com>
2015-01-22 17:43:57 +0100

    Big refactoring of the overall code structure

--- server.js
+++ server.js
@@ -5,39 +5,28 @@ var config = require('./lib/config/config.json');
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
 
 var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var session      = require('express-session');
 var path 		 = require('path'); 
 
 // Setting git data refresh
 
 // configuration ===============================================================
-mongoose.connect(config.db.url); // connect to our database
-
-require('./lib/config/passport')(passport); // pass passport for configuration
-
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
 app.use(express.static(path.join(__dirname, 'public'))); // to get local files
-app.use(session({ secret: config.server.secret})); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./lib/app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./lib/app/routes.js')(app); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
